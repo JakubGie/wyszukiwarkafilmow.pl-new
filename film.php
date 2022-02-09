d1d892be447cd50e8a001430694eb7b6e1c80f92
Jakub <jakubgielara@gmail.com>
2022-02-09 17:21:58 +0100

    movie site

--- film.php
+++ film.php
@@ -11,40 +11,45 @@
         <?php include 'elements/head.php'; ?>
 	</head>
 	<body onload="onload()">
-        <?php include 'elements/brand.php'; ?>
+        <div class="position-relative">
+            <div class="bgimg" style="background-image:url('<?php echo $plakat;?>')" class="w-100"></div>
+            <?php include 'elements/brand.php'; ?>
 
-        <div class="movie-page">
-            <div class="bgimg" style="background-image:url('<?php echo $plakat;?>')" class="w-100">s</div>
-            <div class="info">
-                <h1><?php echo $tytul; ?> <small><?php echo $rok_produkcji; ?></small></h1>
-                <p><?php echo $opis; ?></p>
-            </div>
-        </div>
-        
-        <div class="container-fluid">
-            <div class="main">
-                <?php
-                    if($seria!=0)
-                    {
-                        echo '<h2 class="mt-3">KOLEKCJA "'.$seria_nazwa.'"</h2>';
-                        loadMovie("SELECT * FROM filmy WHERE seria=".$seria." ORDER BY czesc_serii ASC",  100, 0, 0);
-                    }
-                ?>
+            <div class="movie-page">
                 
+                <div class="info">
+                    <img src="<?php echo $plakat; ?>">
+                    <h1><?php echo $tytul; ?> <small><?php echo $rok_produkcji; ?></small></h1>
+                    <p><?php echo $opis; ?></p>
+                    <div class="cb"></div>
+                </div>
             </div>
+            
+            <div class="container-fluid bg-wf-dark">
+                <div class="main">
+                    <?php
+                        if($seria!=0)
+                        {
+                            echo '<h2 class="mt-3">KOLEKCJA "'.$seria_nazwa.'"</h2>';
+                            loadMovie("SELECT * FROM filmy WHERE seria=".$seria." ORDER BY czesc_serii ASC",  100, 0, 0);
+                        }
+                    ?>
+                    
+                </div>
+            </div>
+
+            <script>
+                function onload()
+                {
+                    moreInfoWindows();
+                }
+            </script>
+
+            <?php include 'elements/footer.php'; ?>
+
+            <?php include 'elements/jsscripts.php'; ?>
+            
+            <?php include 'elements/end.php'; ?>
         </div>
-
-        <script>
-            function onload()
-            {
-                moreInfoWindows();
-            }
-        </script>
-
-        <?php include 'elements/footer.php'; ?>
-
-        <?php include 'elements/jsscripts.php'; ?>
-        
-        <?php include 'elements/end.php'; ?>
 	</body>
 </html>
